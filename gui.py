02107007c509f48d331894045b17ac2dbf766bf1
john.dev62 <john.dev62@outlook.fr>
2026-07-20 15:52:15 +0200

    Passe en v1.0.1

--- gui.py
+++ gui.py
@@ -38,7 +38,7 @@ from db import Database
 
 APP_TITLE = "PhotoTri"
 DONATE_URL = "https://ko-fi.com/yoshines62000"
-APP_VERSION = "1.0.0"
+APP_VERSION = "1.0.1"
 UPDATE_REPO = "yoshines62000-alt/PhotoTri"
 RELEASES_URL = f"https://github.com/{UPDATE_REPO}/releases/latest"
 THUMBNAIL_SIZE = (150, 150)
